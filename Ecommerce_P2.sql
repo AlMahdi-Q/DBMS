bd8100068c5c3e3c52df920128e4db4ab399e931
AlMahdi-Q <almahdi103@gmail.com>
2025-01-18 13:44:10 +0400

    Update Ecommerce_P2.sql

--- Ecommerce_P2.sql
+++ Ecommerce_P2.sql
@@ -3,8 +3,8 @@ SELECT Name, Color, Price
 FROM Market
 WHERE Year = 2020
 GROUP BY Name
-HAVING Price = MAX(Price);
-
+HAVING Price = MAX(Price)
+UNION
 SELECT Name, Color, Price
 FROM Market
 WHERE Year = 2020
